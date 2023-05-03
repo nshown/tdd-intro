d569c4e464894b32596ae96fbf452016aa2cedb1
Nathan Shown <nshown@instructors.2u.com>
2023-05-03 12:20:59 -0400

    reduced describe statements

--- 01_Coach-Intro/Solved/test/sum.test.js
+++ 01_Coach-Intro/Solved/test/sum.test.js
@@ -2,29 +2,19 @@
 const sum = require('../sum.js');
 
 // A testing suite for Sum is created.
-describe('Sum', () => {
+describe('Testing sum function', () => {
   // A test is created to check that sum correctly returns 2 numbers added together.
-  describe('sum', () => {
-    test('if add 0 and 0 equals 0', () => {
-      expect(sum(0, 0)).toEqual(0);
-    });
+  test('if add 0 and 0 equals 0', () => {
+    expect(sum(0, 0)).toEqual(0);
   });
-});
 
-describe('Sum', () => {
   // A test is created to check that sum correctly returns 2 numbers added together.
-  describe('sum', () => {
-    test('if add 1 and 1 equals 2', () => {
-      expect(sum(1, 1)).toEqual(2);
-    });
+  test('if add 1 and 1 equals 2', () => {
+    expect(sum(1, 1)).toEqual(2);
   });
-});
 
-describe('Sum', () => {
   // A test is created to check that sum correctly returns 2 numbers added together.
-  describe('sum', () => {
-    test('if add 2 and 4 equals 6', () => {
-      expect(sum(2, 4)).toEqual(6);
-    });
+  test('if add 2 and 4 equals 6', () => {
+    expect(sum(2, 4)).toEqual(6);
   });
 });
--- 02_Student-Activity/Solved/test/reverse.test.js
+++ 02_Student-Activity/Solved/test/reverse.test.js
@@ -2,29 +2,20 @@
 const reverseString = require('../reverseString.js');
 
 // A testing suite for Reverse String is created.
-describe('Reverse String', () => {
+describe('Testing the reverseString function', () => {
   // A test is created to check that and empty string returns an empty string.
-  describe('reverseString', () => {
-    test('if reverse string if "" returns ""', () => {
-      expect(reverseString("")).toEqual("");
-    });
+  test('if reverse string if "" returns ""', () => {
+    expect(reverseString("")).toEqual("");
   });
-});
 
-describe('Reverse String', () => {
   // A test is created to check that reversing "hello world" produces "dlrow olleh"
-  describe('reverseString', () => {
-    test('if reverse string of "hello world" returns "dlrow olleh"', () => {
-      expect(reverseString("hello world")).toEqual("dlrow olleh");
-    });
+  test('if reverse string of "hello world" returns "dlrow olleh"', () => {
+    expect(reverseString("hello world")).toEqual("dlrow olleh");
   });
-});
 
-describe('Reverse String', () => {
   // A test is created to check that the string "12345" is reverse to "54321"
-  describe('reverseString', () => {
-    test('sif reverse string of "12345" returns "54321"', () => {
-      expect(reverseString("12345")).toEqual("54321");
-    });
+  test('sif reverse string of "12345" returns "54321"', () => {
+    expect(reverseString("12345")).toEqual("54321");
   });
+
 });
