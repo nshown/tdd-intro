dc0c8c2d253c1c59ecca603a42490a341df3a767
Nathan Shown <nshown@instructors.2u.com>
2023-05-04 06:50:46 -0400

    added semi colons

--- 01_Coach-Intro/Unsolved/traditional.js
+++ 01_Coach-Intro/Unsolved/traditional.js
@@ -2,6 +2,6 @@ function sum(a, b){
     return a + b;
 }
 
-console.log(sum(0, 0) == 0)
-console.log(sum(1, 1) == 2)
-console.log(sum(2, 4) == 6)
+console.log(sum(0, 0) == 0);
+console.log(sum(1, 1) == 2);
+console.log(sum(2, 4) == 6);
